d9330998d6163d2ebd07d71d9223e3ee89e21287
Marty McLaughlin <martymclaugh@gmail.com>
2017-01-25 14:58:18 -0800

    create footer data file

---
+++ api/FooterData.js
@@ -0,0 +1,18 @@
+export const FooterData = [
+  {
+    title: 'Join Us',
+    links: ['Subscribe', 'Pricing Plans', 'Gift Subscriptions', 'Invite a Friend', 'Educators & Students']
+  },
+  {
+    title: 'Watch Everywhere',
+    links: ['Apple Tv', 'Chromecast', 'Roku', 'Android', 'Kindle', 'iPad', 'iPhone']
+  },
+  {
+    title: 'Company',
+    links: ['About', 'Blog', 'Careers', 'Press', 'Submit Your Film', 'Support', 'Contact']
+  },
+  {
+    title: 'Stay Connected',
+    links: ['Facebook', 'Google+', 'Instagram', 'Pinterest', 'Twitter', 'YouTube']
+  }
+]
