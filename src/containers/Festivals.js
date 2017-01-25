ae6e0e98113947fe10553465f192de6dfe06e95e
Marty McLaughlin <martymclaugh@gmail.com>
2017-01-24 19:00:15 -0800

    add banner component

--- src/containers/Festivals.js
+++ src/containers/Festivals.js
@@ -2,16 +2,21 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchJson } from '../actions';
 import NavBar from '../components/NavBar';
+import Banner from '../components/Banner';
 
 class Festivals extends Component {
   componentWillMount(){
     this.props.fetchJson();
   }
   render(){
+    const motto = "If you're looking for the best films from the world's finest film festivals, you've come to the right place."
     return(
       <div>
         <pre>
           <NavBar />
+          <Banner
+            header="Film Festivals"
+            motto={motto}/>
           {JSON.stringify(this.props, null, 2)}
         </pre>
       </div>
