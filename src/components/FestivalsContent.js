6bed7c6065e5a84fca7829924d0204df6cf9275b
Marty McLaughlin <martymclaugh@gmail.com>
2017-01-25 13:32:00 -0800

    add festivals list component

--- src/components/FestivalsContent.js
+++ src/components/FestivalsContent.js
@@ -5,14 +5,23 @@ import FestivalsList from './FestivalsList';
 export default (props) => {
   console.log(props)
   const { all, data, display, upcoming, title } = props
+  const FestivalAlliance = "Fandor Festival Alliance"
+  const motto = "We connect film festivals to their audiences through social buzz, multimedia promotion, great movies, and exciting offers. Go ahead! Check out our partners."
   return (
     <div className="festivals-wrapper">
-      <h3 className="festivals-title">{title}</h3>
       <div className="festivals-content">
-        <FestivalsPreview
+        <div className="festivals-content-title">
+          <h3 className="festivals-title">{title}</h3>
+          <FestivalsPreview
           display={display}
           data={data}/>
-        <FestivalsList />
+        </div>
+        <FestivalsList
+          title={FestivalAlliance}
+          motto={motto}
+          upcoming={upcoming}
+          all={all}
+          data={data} />
       </div>
     </div>
   )
