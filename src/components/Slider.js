0713ba6932f761a17ddd2172be1ccd65ce837f3f
Marty McLaughlin <martymclaugh@gmail.com>
2017-01-25 09:39:13 -0800

    add slider component

---
+++ src/components/Slider.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import Carousel from 'nuka-carousel';
+import _ from 'lodash';
+
+
+function renderSlide(id, data){
+  let current = _.find(data, { id })
+  console.log(current)
+  if (current !== undefined){
+    let acronym = current.name.match(/\b(\w)/g).join('')
+    console.log(acronym)
+    return (
+      <div key={id} className="slide-image">
+        <div className="acronym"><p>{acronym}</p></div>
+        <img src={`http://dmnj1e9obrjaa.cloudfront.net${current.banner_path}`} alt=""/>
+      </div>
+    )
+  }
+}
+
+export default (props) => {
+  const { slideIds, festivals, slideshow } = props
+  console.log(props)
+  const options = {
+    autoplay: true,
+    slidesToShow: 2,
+    width: "1900px",
+    slidewidth: 950,
+    cellAlign: 'center',
+    wrapAround: true,
+    frameOverflow: "show",
+    slideIndex: 3
+  }
+  return (
+    <div className="relative-wrapper">
+    <div className="slider-wrapper">
+      <div className="shade-left"></div>
+      <div className="shade-right"></div>
+      <Carousel {...options}>
+      {slideIds.map((id) => {return renderSlide(id, festivals)})}
+      {renderSlide(slideshow, festivals)}
+      </Carousel>
+      </div>
+    </div>
+  )
+}
